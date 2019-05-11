bfce8d041ab28de40759829f56acde24c8a0061f
Renell Castro <rcskca@gmail.com>
2019-05-11 05:03:25 -0600

    Board is now drawn onto the page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './App.css'
 
 export default class App extends Component {
   state = {
@@ -8,7 +9,11 @@ export default class App extends Component {
   }
 
   componentDidMount() {
-    this.setState({ board: this.createBoard() });
+    this.setState({ board: this.createBoard() }, () => {
+      //this.updateBoard();
+      console.log('board created');
+      this.updateBoard();
+    });
   }
 
   createBoard() {
@@ -19,10 +24,32 @@ export default class App extends Component {
     return board;
   }
 
+  updateBoard() {
+    let { board } = this.state;
+    let canvas = document.getElementById('canvas');
+    let ctx = canvas.getContext('2d');
+
+    canvas.width = window.innerWidth * 0.9;
+    canvas.height = canvas.width / 2;
+
+    let squareWidth = canvas.width / this.state.width;
+    let squareHeight = canvas.height / this.state.height;
+
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    board.forEach((row, x) => {
+      for (let y = 0; y < row.length; y++) {
+        ctx.fillStyle = row[y] === 0 ? 'white' : 'grey';
+        ctx.fillRect(y * squareWidth, x * squareHeight, squareWidth - 0.5, squareHeight - 0.5);
+      }
+    });
+  }
+
   render() {
     return (
       <div class="container-fluid">
-        <div class="row text-center mt-5">
+        <div class="row text-center mt-2">
           <div class="col-12">
             <h1 class="display-4">Conway's Game of Life</h1>
           </div>
