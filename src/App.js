4931a930efb6a753b7f82c7b12f87825f9c20a2b
Renell Castro <rcskca@gmail.com>
2019-05-12 03:27:43 -0600

    Fixed stack overflow error when checking neighbors, added sim controls

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default class App extends Component {
     let width = 45;
     this.state = {
       width: width,
-      height: Math.round(width / 1.8),
+      height: Math.floor(width / 1.8),
       board: this.createBoard(width),
       simIntervalId: null,
       simRunning: false
@@ -17,7 +17,7 @@ export default class App extends Component {
   createBoard(startingWidth) {
     let board = [];
     for (let i = 0; i < startingWidth; i++) {
-      board.push(new Array(Math.round(startingWidth / 1.4)).fill(false));
+      board.push(new Array(Math.floor(startingWidth / 1.8)).fill(false));
     }
     return board;
   }
@@ -33,35 +33,38 @@ export default class App extends Component {
     this.setState({ board: board });
   }
 
-  startSimulation(speedFactor) {
-    let intervalId = window.setInterval(this.simulationCycle, 1000 * speedFactor);
-    this.setState({ simIntervalId: intervalId, simRunning: true });
-  }
+  startSimulation() {
+    let intervalId = window.setInterval(() => {
+      let { board } = this.state;
+      const { width, height } = this.state;
+      let newBoard = this.createBoard(width);
 
-  simulationCycle() {
-    let { board } = this.state;
-    const { width, height } = this.state;
-    for (let x = 0; x < width; x++) {
-      for (let y = 0; y < height; y++) {
-        let neighborCount = this.getCellNeighborCount(x, y);
-        if (board[x][y]) {
-          board[x][y] = !(neighborCount < 2 || neighborCount > 3);
-        } else {
-          board[x][y] = neighborCount === 3;
+      for (let x = 0; x < width; x++) {
+        for (let y = 0; y < height; y++) {
+          let neighborCount = this.getCellNeighborCount(x, y);
+          if (board[x][y]) {
+            newBoard[x][y] = !(neighborCount < 2 || neighborCount > 3);
+          } else {
+            newBoard[x][y] = neighborCount === 3;
+          }
         }
       }
-    }
-    this.setState({ board: board });
+      this.setState({ board: newBoard });
+    }, 1000);
+    this.setState({ simIntervalId: intervalId, simRunning: true });
   }
 
   getCellNeighborCount(currCellX, currCellY) {
     const { board, width, height } = this.state;
     let neighborCount = 0;
 
-    for (let x = currCellX - 1; x <= x + 1; x++) {
-      for (let y = currCellY - 1; y <= y + 1; y++) {
-        const withinXBounds = x >= 0 && x <= width;
-        const withinYBounds = y >= 0 && y <= height;
+    for (let x = currCellX - 1; x <= currCellX + 1; x++) {
+      for (let y = currCellY - 1; y <= currCellY + 1; y++) {
+        if (x === currCellX && y === currCellY) {
+          continue;
+        }
+        const withinXBounds = x >= 0 && x < width;
+        const withinYBounds = y >= 0 && y < height;
         if (withinXBounds && withinYBounds) {
           neighborCount = board[x][y] ? neighborCount + 1 : neighborCount;
         }
@@ -102,19 +105,19 @@ export default class App extends Component {
           <div className="container-fluid">
             <div className="row">
               <div className="col-md-3">
-                <label for="speed-control">Speed</label><br />
+                <label htmlFor="speed-control">Speed</label><br />
                 <input id="speed-control" type="range" min="1.0" max="4.0" step="0.1"></input>
               </div>
               <div className="col-md-3">
                 <div className="btn-group mt-2" role="group">
-                  <button type="button" class="btn btn-secondary" onClick={this.startSimulation.bind(this, 1.5)}>
-                    <i class="fas fa-play"></i> Start
+                  <button type="button" className="btn btn-primary" onClick={this.startSimulation.bind(this)}>
+                    <i className="fas fa-play"></i> Start
                   </button>
-                  <button type="button" class="btn btn-secondary" onClick={this.pauseSimulation.bind(this)}>
-                    <i class="fas fa-pause"></i> Pause
+                  <button type="button" className="btn btn-secondary" onClick={this.pauseSimulation.bind(this)}>
+                    <i className="fas fa-pause"></i> Pause
                   </button>
-                  <button type="button" class="btn btn-secondary" onClick={this.stopSimulation.bind(this)}>
-                    <i class="fas fa-stop"></i> Stop
+                  <button type="button" className="btn btn-danger" onClick={this.stopSimulation.bind(this)}>
+                    <i className="fas fa-stop"></i> Stop
                   </button>
                 </div>
               </div>
