a68dbb90b2cf4867bfbeb8e4f8aef3bdf0d6ae77
Renell Castro <rcskca@gmail.com>
2019-05-12 08:39:30 -0600

    Refactored App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,24 +5,63 @@ import BoardControl from '../BoardControl/BoardControl';
 export default class App extends Component {
   constructor(props) {
     super(props);
-    let width = 45;
-    let heightRatio = 1.8;
+    this.width = 45;
+    this.heightRatio = 1.8;
+    this.height = Math.floor(this.width / this.heightRatio);
     this.state = {
-      width: width,
-      heightRatio: heightRatio,
-      height: Math.floor(width / heightRatio),
-      board: this.createBoard(width, heightRatio),
+      board: this.createBoard(),
       simIntervalId: null,
       simRunning: false
     };
   }
 
-  createBoard(startingWidth, heightRatio) {
+  createBoard() {
     let board = [];
-    for (let i = 0; i < startingWidth; i++) {
-      board.push(new Array(Math.floor(startingWidth / heightRatio)).fill(false));
+    for (let i = 0; i < this.width; i++) {
+      board.push(new Array(Math.floor(this.height)).fill(false));
     }
     return board;
+  }
+
+  startSimulation() {
+    let intervalId = window.setInterval(() => {
+      const newBoard = this.simulateGeneration();
+      this.setState({ board: newBoard });
+    }, 1000);
+    this.setState({ simIntervalId: intervalId, simRunning: true });
+  }
+
+  simulateGeneration() {
+    let { board } = this.state;
+    let newBoard = this.createBoard();
+
+    for (let x = 0; x < this.width; x++) {
+      for (let y = 0; y < this.height; y++) {
+        let neighborCount = this.getCellNeighborCount(x, y);
+        let doesLivingCellDie = !(neighborCount < 2 || neighborCount > 3);
+        let doesDeadCellLive = neighborCount === 3;
+        newBoard[x][y] = board[x][y] ? doesLivingCellDie : doesDeadCellLive;
+      }
+    }
+    return newBoard;
+  }
+
+  getCellNeighborCount(currCellX, currCellY) {
+    const { board } = this.state;
+    let neighborCount = 0;
+
+    for (let x = currCellX - 1; x <= currCellX + 1; x++) {
+      for (let y = currCellY - 1; y <= currCellY + 1; y++) {
+        if (x === currCellX && y === currCellY) {
+          continue;
+        }
+        const withinXBounds = x >= 0 && x < this.width;
+        const withinYBounds = y >= 0 && y < this.height;
+        const cellLiving = withinXBounds && withinYBounds && board[x][y];
+        neighborCount = cellLiving ? neighborCount + 1 : neighborCount;
+      }
+    }
+    return neighborCount;
   }
 
   colorCell(coords) {
@@ -34,46 +73,6 @@ export default class App extends Component {
     let board = this.state.board;
     board[coords.x][coords.y] = !board[coords.x][coords.y];
     this.setState({ board: board });
-  }
-
-  startSimulation() {
-    let intervalId = window.setInterval(() => {
-      let { board } = this.state;
-      const { width, height, heightRatio } = this.state;
-      let newBoard = this.createBoard(width, heightRatio);
-
-      for (let x = 0; x < width; x++) {
-        for (let y = 0; y < height; y++) {
-          let neighborCount = this.getCellNeighborCount(x, y);
-          if (board[x][y]) {
-            newBoard[x][y] = !(neighborCount < 2 || neighborCount > 3);
-          } else {
-            newBoard[x][y] = neighborCount === 3;
-          }
-        }
-      }
-      this.setState({ board: newBoard });
-    }, 1000);
-    this.setState({ simIntervalId: intervalId, simRunning: true });
-  }
-
-  getCellNeighborCount(currCellX, currCellY) {
-    const { board, width, height } = this.state;
-    let neighborCount = 0;
-
-    for (let x = currCellX - 1; x <= currCellX + 1; x++) {
-      for (let y = currCellY - 1; y <= currCellY + 1; y++) {
-        if (x === currCellX && y === currCellY) {
-          continue;
-        }
-        const withinXBounds = x >= 0 && x < width;
-        const withinYBounds = y >= 0 && y < height;
-        if (withinXBounds && withinYBounds) {
-          neighborCount = board[x][y] ? neighborCount + 1 : neighborCount;
-        }
-      }
-    }
-    return neighborCount;
   }
 
   stopSimulation() {
@@ -96,8 +95,8 @@ export default class App extends Component {
           <Board
             canvasClick={this.colorCell.bind(this)}
             board={this.state.board}
-            width={this.state.width}
-            height={this.state.height} />
+            width={this.width}
+            height={this.height} />
 
           <BoardControl 
             start={this.startSimulation.bind(this)}
